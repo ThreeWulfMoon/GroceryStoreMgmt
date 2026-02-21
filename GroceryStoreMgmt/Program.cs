6a8f13bd6e91661f7b21be2accbcba2b72d47161
ThreeWulfMoon <thornmail1st@gmail.com>
2026-02-21 16:17:17 +0800

    Add project files.

---
+++ GroceryStoreMgmt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GroceryStoreMgmt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
